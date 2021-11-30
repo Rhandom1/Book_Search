7577c9543fa0feed8fcd590e92941f504454937b
Shawn McWhorter <73659834+Rhandom1@users.noreply.github.com>
2021-11-29 21:32:49 -0600

    Update to SavedBooks to fix undefined .length

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
+require ("dotenv").config();
+console.log(process.env.MONGODB_URI);
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/googlebooks';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+console.log(`########%%%%%Connecte to ${mongoURI}********`)
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
