be6d8af08e3528ec182a7af8e0379cdfe5409a4b
Shawn McWhorter <73659834+Rhandom1@users.noreply.github.com>
2021-11-29 23:23:04 -0600

    Cleaning up code

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -49,7 +49,7 @@ const SavedBooks = () => {
       </Jumbotron>
       <Container>
         <h2>
-          {userData.savedBooks.length
+          {userData.savedBooks
             ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
