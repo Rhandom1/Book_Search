6f2aa86a82d30186a17b9c407138b60dc99f5bbe
Shawn McWhorter <73659834+Rhandom1@users.noreply.github.com>
2021-11-29 23:34:31 -0600

    Going through server files chasing 400 error from SavedBooks

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,24 +6,16 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id }).select(
-          "-__v -password"
-        );
-
+        const userData = await User.findOne({ _id: context.user._id }).populate("book");
+        console.log(userData)
         return userData;
       }
-
+      console.log('no context.user found')
       throw new AuthenticationError("Not logged in");
     },
   },
 
   Mutation: {
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
-      const token = signToken(user);
-
-      return { token, user };
-    },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
@@ -40,18 +32,20 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
     saveBook: async (parent, { bookData }, context) => {
       if (context.user) {
-        const updatedUser = await User.findByIdAndUpdate(
+        const updatedUser = await User.findOneAndUpdate(
           { _id: context.user._id },
-          { $push: { savedBooks: bookData } },
-          { new: true }
+          { $addToSet: { savedBooks: bookData } },
+          { new: true, runValidators: true }
         );
-
         return updatedUser;
       }
-
-      throw new AuthenticationError("You need to be logged in!");
     },
     removeBook: async (parent, { bookId }, context) => {
       if (context.user) {
