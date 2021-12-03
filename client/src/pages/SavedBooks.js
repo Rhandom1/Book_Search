5273ad8dbc52c32dd971140043369a181f6a957b
Shawn McWhorter <73659834+Rhandom1@users.noreply.github.com>
2021-12-03 11:13:26 -0600

    Cleaning syntax errors

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -54,10 +54,10 @@ const SavedBooks = () => {
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
-          {userData.savedBooks.map((book) => {
+          {userData.savedBooks?.map((book) => {
             return (
               <Card key={book.bookId} border='dark'>
-                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                {book.image ? (<Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />) : null}
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
                   <p className='small'>Authors: {book.authors}</p>
